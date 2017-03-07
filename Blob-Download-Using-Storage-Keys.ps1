d0f7468204d4964672f552ec376a9d219d108ee7
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2017-03-07 11:10:34 -0500

    Add files via upload

---
+++ Blob-Download-Using-Storage-Keys.ps1
@@ -0,0 +1,13 @@
+﻿$StorageAccountKey="<<REMOVED>>"
+$StorageAccountName = "<<REMOVED e.g. mystoragename (not the full DNS name)>>"
+$ContainerName="<<REMOVED lowercasecontainername>>"
+$DestinationFolder="C:\temp"
+
+$Ctx = New-AzureStorageContext –StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+# List all blobs in a container.
+# $blobs = Get-AzureStorageBlob -Container $ContainerName -Context $Ctx
+
+# Download blobs from a container.
+New-Item -Path $DestinationFolder -ItemType Directory -Force | Out-Null
+$blobs | Get-AzureStorageBlobContent -Destination $DestinationFolder -Context $Ctx -Force
---
+++ Blob-Upload-Using-Storage-Keys.ps1
@@ -0,0 +1,9 @@
+﻿$StorageAccountKey="<<REMOVED>>"
+$StorageAccountName = "<<REMOVED e.g. mystoragename (not the full DNS name)>>"
+$ContainerName="<<REMOVED lowercasecontainername>>"
+$filename="C:\Users\guest\Desktop\Invoice.zip"
+$blobName="Invoice.zip"
+
+$Ctx = New-AzureStorageContext –StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+Set-AzureStorageBlobContent -File $filename -Container $ContainerName -Blob $blobName -Context $Ctx -Force
+
